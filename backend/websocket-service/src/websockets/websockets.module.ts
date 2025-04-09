b0c06207b7a1116ff4ff2bed4b8a20662b194f69
adminaitor <aitorpj93@gmail.com>
2025-04-10 01:03:32 +0200

    fix: OrdersArrayService añadido a los providers de websockets.module

--- backend/websocket-service/src/websockets/websockets.module.ts
+++ backend/websocket-service/src/websockets/websockets.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { WebsocketGateway } from './websocket.gateway';
 import { WebSocketService } from './websocket.service';
 import { WebsocketController } from './websocket.controller';
+import { OrdersArrayService } from 'src/components/ordersArraySingleton.service';
 
 @Module({
-  providers: [WebsocketGateway, WebSocketService],
+  providers: [WebsocketGateway, WebSocketService, OrdersArrayService],
   controllers: [WebsocketController],
 })
 export class GatewayModule {}
