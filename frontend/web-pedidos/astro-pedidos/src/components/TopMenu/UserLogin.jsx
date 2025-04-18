6d6f7ed3eac1a2f511514614776320f445fa403b
adminaitor <aitorpj93@gmail.com>
2025-04-18 18:22:39 +0200

    refactorizando componente LoginModal y corrigiendo errores semanticos

--- frontend/web-pedidos/astro-pedidos/src/components/TopMenu/UserLogin.jsx
+++ frontend/web-pedidos/astro-pedidos/src/components/TopMenu/UserLogin.jsx
@@ -1,5 +1,6 @@
 import "./userLoginStyle.css"
 import { useEffect, useRef, useState } from "react";
+import LoginModal from "./LoginModal";
 
 
 export default function UserLogin( {apiAuthUrl} ) {
@@ -61,8 +62,6 @@ export default function UserLogin( {apiAuthUrl} ) {
     const handleClick = () => {
         if(!userLogin) {
             setShowModal(true);
-        } else {
-            console.log("usuario ya logado");
         }
     }
 
@@ -87,55 +86,14 @@ export default function UserLogin( {apiAuthUrl} ) {
 
             {/* Modal de Login*/}
             {showModal && (
-                <dialog ref={modalRef}>
-                    <div className="login-container">
-
-                        <div className="close-button-container">
-                            <button className="close-button" onClick={closeModal}>
-                                <img src="/close.png" alt="botón cierre" className="close-button-image" />
-                            </button>
-                        </div>
-
-                        <form onSubmit={handleLogin} className="login-form-container">
-                            <p className="login-text">Inicia Sesión</p>
-
-                                <label htmlFor="username" className="login-label">usuario</label>
-                                <input
-                                    type="text"
-                                    name="username"
-                                    id="username"
-                                    className="login-input"
-                                    onChange={ (e) => setUsername(e.target.value)}
-                                /><br/>
-
-                                <label htmlFor="password" className="login-label">contraseña</label>
-                                <input
-                                    type="password"
-                                    name="password" 
-                                    id="password"
-                                    className="login-input"
-                                    onChange={ (e) => setPassword(e.target.value)}
-                                /><br/>
-
-                                <input type="submit" value="ENVIAR" className="login-submit"/>
-
-                        </form>
-                        <a href="/forgotPassword">¿Olvidaste la contraseña?</a>
-
-                        <div className="error-container">
-                            <p className="error-message">{errorMessage}</p>
-                        </div>
-                        <div className="register">
-                            <p>¿Todavía no tienes cuenta?</p>
-                            <a href="/example">Regístrate</a>
-                        </div>
-
-                    </div>
-
-                </dialog>
-                )
-                
-            }
+                <LoginModal
+                    onClose={closeModal}
+                    onSubmit={handleLogin}
+                    errorMessage={errorMessage}
+                    setUsername={setUsername}
+                    setPassword={setPassword}
+                />
+            )}
         </>
     );
 }
