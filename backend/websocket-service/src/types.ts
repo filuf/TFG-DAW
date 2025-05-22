d39759fc6315243c87c68499fd0558566548f1f0
adminaitor <aitorpj93@gmail.com>
2025-05-23 00:45:42 +0200

    metodo de servicio para verificar si el jwt del header pertenece a un usuario con rol SYSTEM

--- backend/websocket-service/src/types.ts
+++ backend/websocket-service/src/types.ts
@@ -19,8 +19,22 @@ type Result = {
   error?: string;
 };
 
+type JwtUserDetailsAuthorities = {
+  authorities: authority[];
+};
+
+type authority = 'SYSTEM' | 'USER' | 'ADMIN';
+
 // Tipos inferidos de los esquemas
 type Pedido = z.infer<typeof PedidoSchema>;
 type Producto = z.infer<typeof ProductoSchema>;
 
-export { Pedido, Producto, ProductoSchema, PedidoSchema, Result };
+export {
+  Pedido,
+  Producto,
+  ProductoSchema,
+  PedidoSchema,
+  Result,
+  JwtUserDetailsAuthorities,
+  authority,
+};
