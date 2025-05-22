fbfe6028f468962d5c3fa4d868dcadaabc7faa3c
adminaitor <aitorpj93@gmail.com>
2025-05-23 00:40:59 +0200

    añade dependencia y configuración necesaria para cargar un .env

--- backend/websocket-service/src/app.module.ts
+++ backend/websocket-service/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GatewayModule } from './websockets/websockets.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GatewayModule],
+  imports: [
+    GatewayModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
