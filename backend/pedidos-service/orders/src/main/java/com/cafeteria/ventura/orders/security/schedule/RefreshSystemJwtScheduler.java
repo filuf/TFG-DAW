673f1aa3f8e18d37f8d330d1c9b7c71f78f9acd6
adminaitor <aitorpj93@gmail.com>
2025-05-22 18:22:42 +0200

    cambio de anotación de configuration a component

--- backend/pedidos-service/orders/src/main/java/com/cafeteria/ventura/orders/security/schedule/RefreshSystemJwtScheduler.java
+++ backend/pedidos-service/orders/src/main/java/com/cafeteria/ventura/orders/security/schedule/RefreshSystemJwtScheduler.java
@@ -6,15 +6,15 @@ import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 @Slf4j
 @RequiredArgsConstructor
 @Getter
 @EnableScheduling
-@Configuration
+@Component
 public class RefreshSystemJwtScheduler {
 
     private final SpringAuthWebhook authWebhook;
